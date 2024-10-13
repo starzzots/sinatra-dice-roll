b3a48c7aa109a6c8ddf97422530e3e1256a8faee
Kyle Verwey <108106818+starzzots@users.noreply.github.com>
2024-10-13 15:24:04 +0000

    added all dice path and working

--- dice.rb
+++ dice.rb
@@ -4,7 +4,7 @@ require "sinatra"
 require "sinatra/reloader" # so I dont have to ctrl + c everytime I make a change
 
 get("/") do
-  "Hello World"
+  "Hello world"
 end
 
 get("/zebra") do
@@ -13,4 +13,48 @@ end
 
 get("/giraffe") do
   "Hopefully this shows up without having to restart the server 🤞🏾"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/2/10") do
+  first_die = rand(1..6)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d10</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/1/20") do
+  first_die = rand(1)
+  second_die = rand(1..20)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>1d20</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/5/4") do
+  first_die = rand(1..5)
+  second_die = rand(1..4)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>5d4</h1>
+   <p>#{outcome}</p>"
 end
